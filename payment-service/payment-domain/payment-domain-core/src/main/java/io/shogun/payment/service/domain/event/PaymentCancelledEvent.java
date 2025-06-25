3f1671af7d5a2a2a180aa1e03dae89324aa6023a
shogun <shoguncrypto93@gmail.com>
2025-06-25 18:08:17 +0700

    refactor: refactor payment domain module

--- payment-service/payment-domain/payment-domain-core/src/main/java/io/shogun/payment/service/domain/event/PaymentCancelledEvent.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/io/shogun/payment/service/domain/event/PaymentCancelledEvent.java
@@ -5,16 +5,12 @@ import io.shogun.payment.service.domain.entity.Payment;
 
 import java.time.ZonedDateTime;
 import java.util.Collections;
+import java.util.List;
 
 public class PaymentCancelledEvent extends PaymentEvent {
-
-    private final DomainEventPublisher<PaymentCancelledEvent> paymentCancelledEventDomainEventPublisher;
-
     public PaymentCancelledEvent(Payment payment,
                                  ZonedDateTime createdAt,
-                                 DomainEventPublisher<PaymentCancelledEvent>
-                                         paymentCancelledEventDomainEventPublisher) {
+                                 List<String> failureMessages) {
         super(payment, createdAt, Collections.emptyList());
-        this.paymentCancelledEventDomainEventPublisher = paymentCancelledEventDomainEventPublisher;
     }
 }
--- payment-service/payment-domain/payment-domain-core/src/main/java/io/shogun/payment/service/domain/event/PaymentCompletedEvent.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/io/shogun/payment/service/domain/event/PaymentCompletedEvent.java
@@ -1,19 +1,13 @@
 package io.shogun.payment.service.domain.event;
 
-import io.shogun.domain.event.publisher.DomainEventPublisher;
 import io.shogun.payment.service.domain.entity.Payment;
 
 import java.time.ZonedDateTime;
 import java.util.Collections;
 
 public class PaymentCompletedEvent extends PaymentEvent {
-
-    private final DomainEventPublisher<PaymentCompletedEvent> paymentCompletedEventDomainEventPublisher;
-
     public PaymentCompletedEvent(Payment payment,
-                                 ZonedDateTime createdAt,
-                                 DomainEventPublisher<PaymentCompletedEvent> paymentCompletedEventDomainEventPublisher) {
+                                 ZonedDateTime createdAt) {
         super(payment, createdAt, Collections.emptyList());
-        this.paymentCompletedEventDomainEventPublisher = paymentCompletedEventDomainEventPublisher;
     }
 }
